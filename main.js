7b7176f05b3036e8db8235a8886eca8606ce3e97
vitor-222 <gouveia.aguiar@escola.pr.gov.br>
2026-08-14 08:09:11 -0300

    #

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  const numetoSenha=document.querySelector('.parametro-senha__titulo');
 
- numero.textContent= 5;
+ numero.textContent =5;
 
  const botoes = document.querySelector9('.parametro-senha__botao');
 
