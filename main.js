003d529143312875483505915499513e61f7d943
vitor-222 <gouveia.aguiar@escola.pr.gov.br>
2026-08-14 08:55:35 -0300

    #

--- main.js
+++ main.js
@@ -5,11 +5,16 @@
  const botoes = document.querySelectorAll('.parametro-senha__botao');
 
  botoes[0].onlick = diminuiTamanho;
+  botoes[1].onlick = aumentaTamanho;
+ 
  fuction diminuiTamanho(){
 tamanhoSenha = tamanhoSenha-1;
  numeroSenha.textContent = tamanhoSenha;
  }
-  
+ function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha+1; 
+    numeroSenha. textContent= tamanhoSenha
+ } 
     console.console.log(botoes);    
 
  
